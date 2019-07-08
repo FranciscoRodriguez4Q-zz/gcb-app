65b7cc15d6f0e7a473aa90bb03d53b8972a0609a
503065981 <balvinderkaur.bal@ge.com>
2019-07-09 00:55:58 +0530

    renaming gcbapi to gcb for deployments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   //APP_BASE_URL_SERVICE_ENDPOINT: "http://localhost:8083/esam",
   //APP_BASE_URL_ENDPOINT: "http://localhost:4200",
   APP_BASE_URL_ENDPOINT: "http://stage-gcb.corporate.ge.com:8080/gcb",
-  APP_BASE_URL_SERVICE_ENDPOINT: "http://stage-gcb.corporate.ge.com:8080/gcbapi",
+  APP_BASE_URL_SERVICE_ENDPOINT: "http://stage-gcb.corporate.ge.com:8080/gcb",
    APP_BILLHUB_URL_SERVICE_ENDPOINT: "http://stage-billhub.corporate.ge.com:8080/esam",
    APP_BILLHUB_URL_UI_ENDPOINT :  "http://stage-billhub.corporate.ge.com:8080/BillHub/index.html#",
 
