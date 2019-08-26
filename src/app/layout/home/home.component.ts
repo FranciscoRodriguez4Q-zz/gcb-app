41182434b6db666d5727fb1e6ef48d05d9976616
Yogita Temkar <ytemkar@igategegdc.com>
2019-08-26 20:06:32 +0530

    Tree View Changes for Product and Country

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -13,78 +13,40 @@ export class HomeComponent implements OnInit {
   expandingTree: Tree;
 
   treeData: TreeNode[];
-
+  
   loading: boolean;
 
   files: any ={
 };
+data:any;
 
   constructor(private homeService: HomeService) { }
 
   ngOnInit() {
       this.loading = true;
-      
-      // this.homeService.getTreeViewData().subscribe(files => {
-      //     this.treeData = [{
-      //         label: 'Root',
-      //         children: files
-      //     }];
-      // });
-
-
-      this.treeData =   this.files = [
-            {
+      this.homeService.getTreeViewData().subscribe(files => {
+         this.data=files; 
+         console.log(this.data[0].children);
+         this.treeData=[{
+            
                 "label": "Product",
-                "data": "Product Folder",
+                "data": "Product",
                 "expandedIcon": "fa fa-folder-open",
                 "collapsedIcon": "fa fa-folder",
-                "children": [{
-                        "label": "Gotems",
-                        "data": "Work Folder",
-                        "expandedIcon": "fa fa-folder-open",
-                        "collapsedIcon": "fa fa-folder",
-                        "children": [{"label": "Audio Conferencing", "icon": "fa fa-file-text-o", "data": "Audio Conferencing"},
-                                     {"label": "Cloud Services", "icon": "fa fa-file-text-o", "data": "Cloud Services"}]
-                    },
-                    {
-                        "label": "Telecom",
-                        "data": "Home Folder",
-                        "expandedIcon": "fa fa-folder-open",
-                        "collapsedIcon": "fa fa-folder",
-                        "children": [{"label": "COMMERCIAL - CREDIT - ADJUSTMENT", "icon": "fa fa-file-text-o", "data": "COMMERCIAL - CREDIT - ADJUSTMENT"}]
-                    }]
-            },
-            {
-                "label": "Vendor",
-                "data": "Vendor",
-                "expandedIcon": "fa fa-folder-open",
-                "collapsedIcon": "fa fa-folder",
-                "children": [
-                    {"label": "AT&T", "icon": "fa fa-file-image-o", "data": "Barcelona Photo"},
-                    {"label": "Vodafone", "icon": "fa fa-file-image-o", "data": "PrimeFaces Logo"},
-                    {"label": "CISCO", "icon": "fa fa-file-image-o", "data": "PrimeUI Logo"}]
-            },
-            {
-                "label": "Country",
-                "data": "Country",
-                "expandedIcon": "fa fa-folder-open",
-                "collapsedIcon": "fa fa-folder",
-                "children": [{
-                        "label": "India",
-                        "data": "India",
-                        "children": [{"label": "Karnataka", "icon": "fa fa-file-video-o", "data": "Karnataka"}, 
-                         {"label": "Maharashtra", "icon": "fa fa-file-video-o", "data": "Maharashtra"}]
-                    },
-                    {
-                        "label": "USA",
-                        "data": "USA",
-                        "children": [{"label": "Ohio", "icon": "fa fa-file-video-o", "data": "Ohio"}, 
-                        {"label": "New York", "icon": "fa fa-file-video-o", "data": "New York"}]
-                    }]
-            }
-        ];
-
-    }
+                "children": this.data[0].children
+            
+          },
+          {
+            
+            "label": "Country",
+            "data": "Country",
+            "expandedIcon": "fa fa-folder-open",
+            "collapsedIcon": "fa fa-folder",
+            "children": this.data[1].children
+        
+      }]
+   });
+}
 
 
   private expandRecursive(node:TreeNode, isExpand:boolean){
--- src/app/layout/home/home.service.ts
+++ src/app/layout/home/home.service.ts
@@ -5,14 +5,47 @@ import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import {  } from 'rxjs';
+import { forkJoin } from 'rxjs';
+import { map,delay,catchError,tap} from 'rxjs/operators';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
+
+    private _product:any;
+    private _country: any;
+
   constructor(private http: HttpClient) { }
 
-  public getTreeViewData(): Observable<Object> {
-    return this.http.get("http://localhost:4200/assets/data/home/TreeData.json");
-  }
+  // public getTreeViewData(): Observable<Object> {
+  //  // return this.http.get("http://3.209.31.137:8081/gcbapi/getLocationTree");
+  //   return this.http.get("http://localhost:8080/gcbapi/billProcess");
+  // }
+
+  public getTreeViewData():Observable<Object> { 
+    let product = this.http.get("http://localhost:8080/gcbapi/productTree");
+    let country = this.http.get("http://localhost:8080/gcbapi/locationTree");
+    
+    // Using forkjoin property of observable to get multiple service values based on array values.
+    return forkJoin([product,country])
+          .pipe(
+            tap(results => this.setTreeViewData(results)),
+            catchError(error => this.handleError(error))
+          );    
+}
+
+private setTreeViewData(getTreeViewData:any){ 
+  this._product = getTreeViewData[0];
+  this._country = getTreeViewData[1];
+}
+
+
+private handleError(error){
+  return throwError(error + "UrlConstants.SERVER_ERROR");
+ }
+  
 }
