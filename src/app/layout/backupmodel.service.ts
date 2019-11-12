7de35006986d977e253bce6c90bb86279f3d70af
Gonzalez, Josue <503156667@ge.com>
2019-11-12 09:04:38 -0600

    item 70 working

---
+++ src/app/layout/backupmodel.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackupModelService {
+
+  public productTabModel;
+  public serviceTypeTabModel;
+  public vendorTabModel;
+  public vendorConfigTabModel;
+  public buyerTabModel;
+  public banTabModel;
+
+  constructor() {}
+
+}
