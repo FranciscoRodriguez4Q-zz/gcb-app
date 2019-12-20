31ab4a35ecc3c63cbf200caf97bccbd3f28aa2a9
Gonzalez, Josue <503156667@ge.com>
2019-12-20 09:55:46 -0600

    change to https for billhub

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,8 +9,8 @@ export const environment = {
   APP_BASE_URL_ENDPOINT: "http://stage-gcb.corporate.ge.com/gcb",
   APP_BASE_URL_SERVICE_ENDPOINT: "http://stage-gcb.corporate.ge.com/gcb",
 
-  APP_BILLHUB_URL_SERVICE_ENDPOINT: "http://stage-billhub.corporate.ge.com:8080/esam",
-  APP_BILLHUB_URL_UI_ENDPOINT :  "http://stage-billhub.corporate.ge.com:8080/BillHub/index.html#",
+  APP_BILLHUB_URL_SERVICE_ENDPOINT: "https://stage-billhub.corporate.ge.com/esam",
+  APP_BILLHUB_URL_UI_ENDPOINT :  "https://stage-billhub.corporate.ge.com/BillHub/index.html#",
 
   // APP_BASE_URL_ENDPOINT: "http://gcb.corporate.ge.com/gcb",
   // APP_BASE_URL_SERVICE_ENDPOINT: "http://gcb.corporate.ge.com/gcb",
